59175e8f15bc3e82f90d8516cfdfc25989979236
alan-lam <alanlam00@gmail.com>
2018-09-15 23:48:51 -0700

    get and show ip address

--- app/src/main/java/com/example/alan/chatterbox/MainActivity.java
+++ app/src/main/java/com/example/alan/chatterbox/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.alan.chatterbox;
 
+import android.net.wifi.WifiManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.format.Formatter;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        WifiManager wm = (WifiManager) getApplicationContext().getSystemService(WIFI_SERVICE);
+        String ip = Formatter.formatIpAddress(wm.getConnectionInfo().getIpAddress());
+        TextView ip_address_tv = findViewById(R.id.ip_address);
+        ip_address_tv.setText(ip);
     }
 }
