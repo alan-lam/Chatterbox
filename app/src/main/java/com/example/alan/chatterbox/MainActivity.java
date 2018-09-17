d6547159f8239935b82b681a27cf465fb3685f0e
alan-lam <alanlam00@gmail.com>
2018-09-17 14:36:13 -0700

    update

--- app/src/main/java/com/example/alan/chatterbox/MainActivity.java
+++ app/src/main/java/com/example/alan/chatterbox/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.alan.chatterbox;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import java.net.Inet4Address;
@@ -13,7 +15,7 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate (Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -24,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
                 List<InetAddress> addrs = Collections.list(intf.getInetAddresses());
                 for (InetAddress addr : addrs) {
                     if (!addr.isLoopbackAddress() && addr instanceof Inet4Address) {
-                        ip = addr.getHostAddress().toString();
+                        ip = addr.getHostAddress();
                     }
                 }
             }
@@ -34,5 +36,27 @@ public class MainActivity extends AppCompatActivity {
         }
         TextView ip_address_tv = findViewById(R.id.ip_address);
         ip_address_tv.setText(ip);
+
+        Button start_server = findViewById(R.id.start_server);
+        start_server.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick (View view) {
+                /*
+                Intent ...
+                startActivity(...);
+                 */
+            }
+        });
+
+        Button connect_server = findViewById(R.id.connect_server);
+        connect_server.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick (View view) {
+                /*
+                Intent ...
+                startActivity(...);
+                 */
+            }
+        });
     }
 }
